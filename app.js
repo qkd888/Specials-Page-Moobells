323ae364aeb124947ce89c03046fc5fe1633041f
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2021-01-28 21:20:07 +0200

    update slider .js and css

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-var flkty = new Flickity('.carousel');
 
-var previousButton = document.querySelector('.button--previous');
-previousButton.addEventListener('click', function () {
-    flkty.previous();
-});
+(() => {
+    if (document.querySelector('.carousel')) {
+      let pSlides = document.querySelector('.carousel')
+      let partnerFlkty = new Flickity(pSlides, {
+        draggable: true,
+        wrapAround: false,
+        prevNextButtons: true,
+        pageDots: true,
+        adaptiveHeight: false,
+        autoPlay: 4000,
+        cellAlign: 'center',
+        groupCells: 4,
+        arrowShape: {
+          x0: 10,
+          x1: 60, y1: 50,
+          x2: 70, y2: 40,
+          x3: 30
+        },
+      });
+    }
+  })();
 
-var previousWrappedButton = document.querySelector('.button--previous-wrapped');
-previousWrappedButton.addEventListener('click', function () {
-    flkty.previous(true);
-});
+
